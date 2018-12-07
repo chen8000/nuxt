d8fbf8e0908090a1360a22979d982fe06d96a419
zhanghui.chen <13126618000@163.com>
2018-12-07 15:34:06 +0800

    nuxt

---
+++ pages/user/_id/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>这个页面的参数是：{{$route.params.id}}</div>
+</template>
+
+<script>
+export default {
+    validate (data){
+
+        return /^\d+$/.test(data.params.id)
+
+    }
+}
+</script>
+
---
+++ pages/user/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>这是子级</h1>
+    </div>
+</template>
+
